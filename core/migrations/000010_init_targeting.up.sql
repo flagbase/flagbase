6d1eb1a3c26ca5ecfa0adf6757afa6e88d26e89f
Chris Joy <chrisjoyofficial@gmail.com>
2020-12-27 12:10:52 +1100

    OSS-54: update targeting migrations and swagger spec

--- core/migrations/000010_init_targeting.up.sql
+++ core/migrations/000010_init_targeting.up.sql
@@ -3,20 +3,16 @@ BEGIN;
 -- --------------------------
 -- Simple Targeting Algorithm
 -- --------------------------
--- state (on, off)
---  on ->
+-- enabled (TRUE, FALSE)
+--  TRUE  ->
 --    if rules exist  -> derive from targeting_rule
 --    else            -> derive from targeting_weights
---  off  ->  use fallthrough_variation
+--  FALSE ->  use fallthrough_variation
 --
-CREATE TYPE targeting_state AS ENUM (
-  'on',
-  'off'
-);
 CREATE TABLE targeting (
   id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
   -- attributes
-  state targeting_state,
+  enabled BOOLEAN DEFAULT FALSE NOT NULL,
   -- references
   fallthrough_variation_id UUID REFERENCES variation (id),
   flag_id UUID REFERENCES flag (id),
@@ -40,8 +36,8 @@ CREATE TABLE targeting_rule (
   id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
   -- attributes
   key resource_key,
-  type targeting_rule_type,
-  condition targeting_rule_condition,
+  type targeting_rule_type NOT NULL,
+  condition targeting_rule_condition NOT NULL,
   -- references
   identity_id UUID REFERENCES identity (id),
   segment_id UUID REFERENCES segment (id),
@@ -51,22 +47,44 @@ CREATE TABLE targeting_rule (
   CONSTRAINT targeting_rule_key UNIQUE(key, targeting_id)
 );
 
+
+-- --------------------------
+-- Targeting Weights
+-- --------------------------
+-- type (fallthrough, rule)
+--  fallthrough ->
+--      % rollout ON targeting.fallthrough_variation
+--  rule -> % rollout
+--      % rollout ON targeting_rule.[targeting_rule_type].variation
+--
+CREATE TYPE targeting_weight_type AS ENUM (
+  'fallthrough',
+  'rule'
+);
 CREATE TABLE targeting_weight (
-  PRIMARY KEY (variation_id, targeting_id),
+  PRIMARY KEY (targeting_id, targeting_rule_id, variation_id),
   -- attributes
-  weight INT,
+  type targeting_weight_type NOT NULL,
+  weight INT NOT NULL,
   -- references
-  variation_id UUID REFERENCES variation (id),
-  targeting_id UUID REFERENCES targeting (id)
+  targeting_id UUID REFERENCES targeting (id),
+  targeting_rule_id UUID REFERENCES targeting_rule (id),
+  variation_id UUID REFERENCES variation (id) -- variation that is being targeted
 );
 
 -- verify targeting weight doesn't exceed 100
 CREATE FUNCTION targeting_weight_verification() RETURNS TRIGGER AS $$
 BEGIN
-    IF coalesce((select sum(weight)
-                from targeting_weight
-                where targeting_id = NEW.targeting_id), 0)
-        + coalesce(NEW.weight, 0) > 100 THEN
+    IF coalesce(
+      (select sum(weight)
+        from
+          targeting_weight
+        where
+          type = NEW.type AND (
+            targeting_id = NEW.targeting_id OR
+            targeting_rule_id = NEW.targeting_rule_id
+          )
+      ), 0) + coalesce(NEW.weight, 0) > 100 THEN
       RAISE EXCEPTION 'targeting_weight aggregation exceeds 100%', NEW.targeting_id;
     END IF;
   RETURN NEW;
