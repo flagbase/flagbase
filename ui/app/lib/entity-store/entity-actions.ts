a1e5e4172952dcaf2962f3686570b359c58f2089
Chris Joy <chrisjoyofficial@gmail.com>
2021-03-06 12:11:54 +1100

    OSS-37: added clearEntities action to entity-actions

--- ui/app/lib/entity-store/entity-actions.ts
+++ ui/app/lib/entity-store/entity-actions.ts
@@ -1,9 +1,10 @@
-import { Entity, EntityStore } from './entity-store';
+import { Entity, EntityStore } from "./entity-store";
 
 export type EntityActions<T> = {
   addEntity: (entity: Entity<T>) => void;
   addEntities: (entities: { [entityId: string]: Entity<T> }) => void;
   removeEntity: (entityId: string) => void;
+  clearEntities: () => void;
   setLoading: (isLoading: boolean) => void;
   setError: (error: string) => void;
   setSelectedEntityId: (selectedEntityId: string) => void;
@@ -15,23 +16,30 @@ export const createActions = <T>(
   state: EntityStore<T>,
   setState: (state: EntityStore<T>) => void
 ): EntityActions<T> => ({
-    addEntity: (entity: Entity<T>) =>
-      setState({
-        ...state,
-        entities: { ...state.entities, [entity.id]: entity }
-      }),
-    addEntities: (entities: { [entityId: string]: Entity<T> }) =>
-      setState({ ...state, entities: { ...state.entities, ...entities } }),
-    removeEntity: (entityId: string) =>
-      setState({
-        ...state,
-        entities: { ...state.entities, [entityId]: undefined }
-      }),
-    setLoading: (isLoading = true) => setState({ ...state, isLoading }),
-    setError: (error: string) => setState({ ...state, error }),
-    setSelectedEntityId: (selectedEntityId: string) =>
-      setState({ ...state, selectedEntityId }),
-    selectEntity: (entity: Entity<T>) =>
-      setState({ ...state, selectedEntityId: entity.id }),
-    getEntity: (entityId: string): Entity<T> | undefined => state.entities[entityId]
-  });
+  addEntity: (entity: Entity<T>) =>
+    setState({
+      ...state,
+      entities: { ...state.entities, [entity.id]: entity },
+    }),
+  addEntities: (entities: { [entityId: string]: Entity<T> }) =>
+    setState({ ...state, entities: { ...state.entities, ...entities } }),
+  removeEntity: (entityId: string) =>
+    setState({
+      ...state,
+      entities: { ...state.entities, [entityId]: undefined },
+    }),
+  clearEntities: () =>
+    setState({
+      ...state,
+      entities: {},
+      selectedEntityId: null,
+    }),
+  setLoading: (isLoading = true) => setState({ ...state, isLoading }),
+  setError: (error: string) => setState({ ...state, error }),
+  setSelectedEntityId: (selectedEntityId: string) =>
+    setState({ ...state, selectedEntityId }),
+  selectEntity: (entity: Entity<T>) =>
+    setState({ ...state, selectedEntityId: entity.id }),
+  getEntity: (entityId: string): Entity<T> | undefined =>
+    state.entities[entityId],
+});
