b5a833e1d5b233e8dd4d1f1025d7dc3681619e24
Chris Joy <chrisjoyofficial@gmail.com>
2021-03-06 12:43:07 +1100

    OSS-68: update test imports

--- ui/components/Button/Button.spec.tsx
+++ ui/components/Button/Button.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Button from './button';
+import Button from '../button';
 
 test('button should renders', () => {
   const { getByText } = render(<Button>ButtonContent</Button>);
