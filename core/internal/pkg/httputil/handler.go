9f1a06611c5f205cec38ff5ded1fbcfc6a7e78d0
Chris Joy <chrisjoyofficial@gmail.com>
2021-03-27 20:12:36 +1100

    REFACTOR: remove redundant args from handleFn

--- core/internal/pkg/httputil/handler.go
+++ core/internal/pkg/httputil/handler.go
@@ -9,7 +9,7 @@ import (
 // Handler passes the server context into a HTTP handler fn
 func Handler(
 	sctx *srv.Ctx,
-	handleFn func(sctx *srv.Ctx, ctx *gin.Context),
+	handleFn func(*srv.Ctx, *gin.Context),
 ) func(gctx *gin.Context) {
 	return func(gctx *gin.Context) {
 		handleFn(sctx, gctx)
