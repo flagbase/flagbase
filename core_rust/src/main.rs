5e598ea9418e271ff0b92ab194adb1c0d2705d67
Anuj <anuj@flagbase.com>
2021-03-28 22:04:25 +1100

    Adds docker related files for core_rust

--- core_rust/src/main.rs
+++ core_rust/src/main.rs
@@ -1,5 +1,26 @@
+use actix_web::{get, post, web, App, middleware::Logger, HttpResponse, HttpServer, Responder};
+use log::LevelFilter;
+
+use env_logger::Env;
+use env_logger::Builder;
+
 mod health_check;
 
-fn main() {
-    println!("Hello, World!");
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("G'day, World!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(hello)
+    })
+    .bind("0.0.0.0:5051")?
+    .run()
+    .await
 }
