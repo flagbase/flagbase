8b5303976af008df46c7dc1e57f788cdd9ae1fa8
Chris Joy <chrisjoyofficial@gmail.com>
2021-04-14 23:28:08 +1000

    OSS-85: add polling pkg

---
+++ core/internal/pkg/polling/polling_endpoints.go
@@ -0,0 +1,25 @@
+package polling
+
+import (
+	"core/internal/pkg/httpmetrics"
+	"core/internal/pkg/httputil"
+	srv "core/internal/pkg/server"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ApplyRoutes applies route from all packages to root handler
+func ApplyRoutes(sctx *srv.Ctx, r *gin.Engine) {
+	httpmetrics.ApplyMetrics(r)
+	routes := r.Group("")
+	routes.GET("/flags/raw", httputil.Handler(sctx, getRawFlags))
+	routes.POST("/flags/evaluated", httputil.Handler(sctx, evaluateFlags))
+}
+
+func getRawFlags(sctx *srv.Ctx, ctx *gin.Context) {
+	// TODO (OSS-81)
+}
+
+func evaluateFlags(sctx *srv.Ctx, ctx *gin.Context) {
+	// TODO (OSS-81)
+}
---
+++ core/internal/pkg/polling/polling_server.go
@@ -0,0 +1,22 @@
+package polling
+
+import (
+	"core/internal/pkg/httpserver"
+	srv "core/internal/pkg/server"
+)
+
+// Config API server configuration
+type Config struct {
+	Host        string
+	PollingPort int
+	Verbose     bool
+}
+
+// New initialize a new HTTP server for polling
+func New(sctx *srv.Ctx, cfg Config) {
+	httpserver.New(sctx, httpserver.Config{
+		Host:     cfg.Host,
+		HTTPPort: cfg.PollingPort,
+		Verbose:  cfg.Verbose,
+	}, ApplyRoutes)
+}
