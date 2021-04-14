529e379e1207a49dce98e014d1dab93990a74365
Chris Joy <chrisjoyofficial@gmail.com>
2021-04-14 23:04:23 +1000

    OSS-85: add httpserver package

--- core/internal/pkg/api/api_server.go
+++ core/internal/pkg/api/api_server.go
@@ -1,11 +1,8 @@
 package api
 
 import (
+	"core/internal/pkg/httpserver"
 	srv "core/internal/pkg/server"
-	"strconv"
-
-	"github.com/gin-contrib/logger"
-	"github.com/gin-gonic/gin"
 )
 
 // Config API server configuration
@@ -17,22 +14,9 @@ type Config struct {
 
 // New initialize a new gin-based HTTP server
 func New(sctx *srv.Ctx, cfg Config) {
-	if !cfg.Verbose {
-		gin.SetMode(gin.ReleaseMode)
-	}
-
-	r := gin.New()
-
-	if cfg.Verbose {
-		r.Use(logger.SetLogger(logger.Config{
-			Logger: sctx.Log.Logger,
-		}))
-	}
-
-	ApplyRoutes(sctx, r)
-
-	err := r.Run(cfg.Host + ":" + strconv.Itoa(cfg.APIPort))
-	if err != nil {
-		sctx.Log.Error.Str("reason", err.Error()).Msg("Unable to start HTTP server")
-	}
+	httpserver.New(sctx, httpserver.Config{
+		Host:     cfg.Host,
+		HTTPPort: cfg.APIPort,
+		Verbose:  cfg.Verbose,
+	}, ApplyRoutes)
 }
