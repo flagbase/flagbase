57c7f9fb07bdc08d222a1e564cb33e64b7dad740
Chris Joy <chrisjoyofficial@gmail.com>
2021-04-25 14:01:11 +1000

    OSS-23: add targting and targetingrule

--- core/internal/app/segmentrule/segmentrule.go
+++ core/internal/app/segmentrule/segmentrule.go
@@ -1,32 +1,19 @@
 package segmentrule
 
-import rsc "core/internal/pkg/resource"
+import (
+	"core/internal/app/ruleoperand"
+	rsc "core/internal/pkg/resource"
+)
 
 // SegmentRule represents a condition used to filter identities for a particular segment
 type SegmentRule struct {
-	ID         rsc.ID   `json:"id"`
-	Key        rsc.Key  `json:"key"`
-	TraitKey   string   `json:"traitKey"`
-	TraitValue string   `json:"traitValue"`
-	Operator   Operator `json:"operator"`
-	Negate     bool     `json:"negate"`
+	ID         rsc.ID               `json:"id"`
+	Key        rsc.Key              `json:"key"`
+	TraitKey   string               `json:"traitKey"`
+	TraitValue string               `json:"traitValue"`
+	Operator   ruleoperand.Operator `json:"operator"`
+	Negate     bool                 `json:"negate"`
 }
-
-// Operator represents an conditional operator (i.e. <operand> <operator> <operand>)
-type Operator string
-
-const (
-	// Equal exact match
-	Equal Operator = "equal"
-	// GreaterThan > (numeric comparisons)
-	GreaterThan Operator = "greater_than"
-	// GreaterThanOrEqual >= (numeric comparisons)
-	GreaterThanOrEqual Operator = "greater_than_or_equal"
-	// Contains is a substring
-	Contains Operator = "contains"
-	// Regex regular expression match
-	Regex Operator = "regex"
-)
 
 // RootArgs arguments for selecting root resource
 type RootArgs struct {
