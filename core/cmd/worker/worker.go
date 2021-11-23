2c7c5924d661626d2137c332b69291f39fd18b66
Chris Joy <chrisjoyofficial@gmail.com>
2021-11-23 23:36:15 +1100

    REFACTOR: merge worker_cmd & worker_flags

--- core/cmd/worker/worker.go
+++ core/cmd/worker/worker.go
@@ -14,6 +14,19 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+const (
+	// ModeFlag Type of worker (api, graphql, streamer, poller)
+	ModeFlag string = "mode"
+	// HostFlag Server host address
+	HostFlag string = "host"
+	// APIPortFlag Port API will operate within
+	APIPortFlag string = "api-port"
+	// StreamerPortFlag Port streamer will operate within
+	StreamerPortFlag string = "streamer-port"
+	// PollingPortFlag Port streamer will operate within
+	PollerPortFlag string = "poller-port"
+)
+
 // Command worker command entry
 var Command cli.Command = cli.Command{
 	Name:        "worker",
