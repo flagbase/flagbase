e98c68146ad9ea5253ebbb2209744a0b4b4877d0
Chris Joy <chrisjoyofficial@gmail.com>
2023-03-25 17:39:04 +1100

    feature: update access model

--- core/internal/app/access/model/access.go
+++ core/internal/app/access/model/access.go
@@ -5,14 +5,17 @@ import rsc "core/internal/pkg/resource"
 // Access is used to represent the relationship between the API user and the service.
 // Access objects are attached to the resources, which are used to authorise users.
 type Access struct {
-	ID          string          `json:"id,omitempty"`
-	Key         rsc.Key         `json:"key,omitempty"`
-	Description rsc.Description `json:"description,omitempty"`
-	Name        rsc.Name        `json:"name,omitempty"`
-	Tags        rsc.Tags        `json:"tags,omitempty"`
-	Secret      string          `json:"secret,omitempty"`
-	Type        string          `json:"type,omitempty"`
-	ExpiresAt   int64           `json:"expiresAt,omitempty"`
+	ID           string          `json:"id,omitempty"`
+	Key          rsc.Key         `json:"key,omitempty"`
+	Description  rsc.Description `json:"description,omitempty"`
+	Name         rsc.Name        `json:"name,omitempty"`
+	Tags         rsc.Tags        `json:"tags,omitempty"`
+	Secret       string          `json:"secret,omitempty"`
+	Type         string          `json:"type,omitempty"`
+	Scope        string          `json:"scope,omitempty"`
+	WorkspaceKey string          `json:"workspaceKey,omitempty"`
+	ProjectKey   string          `json:"projectKey,omitempty"`
+	ExpiresAt    int64           `json:"expiresAt,omitempty"`
 }
 
 // KeySecretPair access secret-key pair, used for login
