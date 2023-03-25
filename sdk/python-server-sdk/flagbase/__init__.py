55c6fa58e08f4e25c1ee59a4214d3544255baa22
Chris Joy <chrisjoyofficial@gmail.com>
2023-03-25 11:52:15 +1100

    feature: publish python server sdk

--- sdk/python-server-sdk/flagbase/__init__.py
+++ sdk/python-server-sdk/flagbase/__init__.py
@@ -2,3 +2,4 @@ from flagbase.client import FlagbaseClient
 from flagbase.config import Config, Mode
 from flagbase.events import EventType
 from flagbase.identity import Identity
+from flagbase.version import __version__
--- sdk/python-server-sdk/setup.py
+++ sdk/python-server-sdk/setup.py
@@ -1,34 +1,29 @@
 from os import path
-from setuptools import setup
+from setuptools import setup, find_packages
+from pathlib import Path
+
+long_description = (Path(__file__).parent / "README.md").read_text()
 
 INSTALL_REQUIRES = [
-    'requests>=2.9.1',
-    'pyyaml>=5.4',
-    'docopt>=0.6.2',
-    'enum34;python_version<"3.4"',
-    'bloom-filter2>=2.0.0',
+    'requests>=2.26.0',
     'xxhash>=3.2.0',
 ]
 
 with open(path.join(path.abspath(path.dirname(__file__)), 'flagbase', 'version.py'), encoding='utf-8') as f:
     exec(f.read())
 
-with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as r:
-    README = r.read()
-
 setup(
     name="flagbase",
     # pylint: disable=undefined-variable
     version=__version__,  # type: ignore
     description='Flagbase Python Server SDK',
-    packages=['flagbase'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='The Flagbase Team',
+    packages=find_packages(),
     author_email='cjoy@flagbase.com',
-    url='https://github.com/flagbase/flagbase/tree/master/sdk/python-server-sdk',
-    install_requires=[
-        'requests',
-        'ua_parser',
-        'ip3country',
-    ],
+    url='https://flagbase.com',
+    install_requires=INSTALL_REQUIRES,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
