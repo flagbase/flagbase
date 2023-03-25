37e8025335734c8d4549349de68da3f263e02edb
Chris Joy <chrisjoyofficial@gmail.com>
2023-03-26 10:05:31 +1100

    feature: enforcing access requirements

--- core/internal/app/access/service/access_service.go
+++ core/internal/app/access/service/access_service.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	accessmodel "core/internal/app/access/model"
 	accessrepo "core/internal/app/access/repository"
-	"core/internal/pkg/auth"
 	"core/internal/pkg/authv2"
 	cons "core/internal/pkg/constants"
 	"core/internal/pkg/jwt"
@@ -14,6 +13,7 @@ import (
 	"core/pkg/patch"
 	res "core/pkg/response"
 	"encoding/json"
+	"fmt"
 )
 
 type Service struct {
@@ -76,6 +76,7 @@ func (s *Service) List(
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// Verify access is authorized
 	acc, err := authv2.Authorize(s.Senv, atk)
 	if err != nil {
 		e.Append(cons.ErrorAuth, err.Error())
@@ -88,21 +89,21 @@ func (s *Service) List(
 	}
 
 	// Enforce access requirements
-	if acc.Type == "admin" && acc.Scope == "workspace" {
+	if acc.Type == rsc.AccessAdmin.String() && acc.Scope == rsc.AccessScopeWorkspace.String() {
 		// if admin and scoped to workspace
 		for _, _r := range r {
 			if _r.WorkspaceKey != acc.WorkspaceKey {
 				_r.ID = "redacted"
 			}
 		}
-	} else if acc.Type == "admin" && acc.Scope == "project" {
+	} else if acc.Type == rsc.AccessAdmin.String() && acc.Scope == rsc.AccessScopeProject.String() {
 		// if admin and scoped to project
 		for _, _r := range r {
 			if _r.WorkspaceKey != acc.WorkspaceKey && _r.ProjectKey != acc.ProjectKey {
 				_r.ID = "redacted"
 			}
 		}
-	} else if acc.Type == "user" || acc.Type == "service" {
+	} else if acc.Type == rsc.AccessUser.String() || acc.Type == rsc.AccessService.String() {
 		for _, _r := range r {
 			if _r.ID != acc.ID {
 				_r.ID = "redacted"
@@ -116,13 +117,9 @@ func (s *Service) List(
 	// Filter eligible items
 	filtered := make([]*accessmodel.Access, 0)
 	for _, _r := range r {
-		// hide secrets
-		_r.Secret = "**************"
+		_r.Secret = "**************" // hide secrets
 		if _r.ID != "redacted" {
-			s.Senv.Log.Debug().Msgf("Keeping: %s", _r.Key)
 			filtered = append(filtered, _r)
-		} else {
-			s.Senv.Log.Debug().Msgf("Redacting: %s", _r.Key)
 		}
 	}
 
@@ -142,6 +139,43 @@ func (s *Service) Create(
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// Verify access is authorized
+	acc, err := authv2.Authorize(s.Senv, atk)
+	if err != nil {
+		e.Append(cons.ErrorAuth, err.Error())
+		return nil, &e
+	}
+
+	// Enforce access requirements
+	if acc.Type == rsc.AccessUser.String() || acc.Type == rsc.AccessService.String() {
+		// users/service is not allowed to create access
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s is not allowed to create access", acc.Type))
+		cancel()
+	} else if acc.Type == rsc.AccessAdmin.String() && i.Scope == rsc.AccessScopeInstance.String() {
+		// admin is only allowed to create scoped access (workspace or project)
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s is not allowed to create instance access (must be root access type)", acc.Type))
+		cancel()
+	} else if acc.Type == rsc.AccessAdmin.String() && i.Scope == rsc.AccessScopeWorkspace.String() && acc.WorkspaceKey != i.WorkspaceKey {
+		// if admin trying to create scoped workspace access out of their scope
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s is not authorised to create workspace outside own scope", acc.Type))
+		cancel()
+	} else if acc.Type == rsc.AccessAdmin.String() && i.Type == rsc.AccessRoot.String() {
+		// admin is not allowed to create root access
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s is not authorised to create root access", acc.Type))
+		cancel()
+	}
+	// otherwise
+	// * create if root with instance scope
+
+	// Valid scope requirements
+	if i.Scope == "project" && (i.WorkspaceKey == "" || i.ProjectKey == "") {
+		e.Append(cons.ErrorInput, "Access scoped in project should contain a both a workspace key and a project key")
+		cancel()
+	} else if i.Scope == "workspace" && i.WorkspaceKey == "" {
+		e.Append(cons.ErrorInput, "Access scoped in workspace should contain a workspace key")
+		cancel()
+	}
+
 	encryptedSecret, err := crypto.Encrypt(i.Secret)
 	if err != nil {
 		e.Append(cons.ErrorCrypto, err.Error())
@@ -150,17 +184,6 @@ func (s *Service) Create(
 
 	originalSecret := i.Secret
 	i.Secret = encryptedSecret
-
-	// Valid scope requirements
-	if i.Scope == "project" && (i.WorkspaceKey == "" || i.ProjectKey == "") {
-		// ACCESS TODO: verify workspace/project exists
-		e.Append(cons.ErrorCrypto, "Access scoped in project should contain a both a workspace key and a project key")
-		cancel()
-	} else if i.Scope == "workspace" && i.WorkspaceKey == "" {
-		// ACCESS TODO: verify workspace exists
-		e.Append(cons.ErrorCrypto, "Access scoped in workspace should contain a workspace key")
-		cancel()
-	}
 
 	r, err := s.AccessRepo.Create(ctx, i)
 	if err != nil {
@@ -183,6 +206,13 @@ func (s *Service) Get(
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// Verify access is authorized
+	acc, err := authv2.Authorize(s.Senv, atk)
+	if err != nil {
+		e.Append(cons.ErrorAuth, err.Error())
+		return nil, &e
+	}
+
 	r, err := s.AccessRepo.Get(ctx, accessmodel.KeySecretPair{
 		Key: a.AccessKey.String(),
 	})
@@ -190,15 +220,22 @@ func (s *Service) Get(
 		e.Append(cons.ErrorNotFound, err.Error())
 	}
 
-	if err := auth.Enforce(
-		s.Senv,
-		atk,
-		r.ID,
-		rsc.Workspace,
-		rsc.AccessService,
-	); err != nil {
-		e.Append(cons.ErrorAuth, err.Error())
+	// Enforce access requirements
+	if acc.Type == rsc.AccessAdmin.String() && acc.Scope == rsc.AccessScopeWorkspace.String() && r.WorkspaceKey != acc.WorkspaceKey {
+		// admin access scoped to workspace is only allowed to get scoped access
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s scoped to a workspace %s is not allowed to retrieve access outside its scope", acc.Type, acc.WorkspaceKey))
+		cancel()
+	} else if acc.Type == rsc.AccessAdmin.String() && acc.Scope == rsc.AccessScopeProject.String() && (r.WorkspaceKey != acc.WorkspaceKey || r.ProjectKey != acc.ProjectKey) {
+		// admin access scoped to project is only allowed to get scoped access
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s scoped to a workspace %s and project %s is not allowed to retrieve access outside its scope", acc.Type, acc.WorkspaceKey, acc.ProjectKey))
+		cancel()
+	} else if (acc.Type == rsc.AccessUser.String() || acc.Type == rsc.AccessService.String()) && acc.ID != r.ID {
+		// user/service access is only allowed to get self
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s is not authorized to retrieve other access", acc.Type))
+		cancel()
 	}
+	// otherwise
+	// * show if root with instance scope
 
 	// hide secret
 	r.Secret = "**************"
@@ -218,22 +255,39 @@ func (s *Service) Update(
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// Verify access is authorized
+	acc, err := authv2.Authorize(s.Senv, atk)
+	if err != nil {
+		e.Append(cons.ErrorAuth, err.Error())
+		return nil, &e
+	}
+
 	r, err := s.AccessRepo.Get(ctx, accessmodel.KeySecretPair{Key: a.AccessKey.String()})
 	if err != nil {
 		e.Append(cons.ErrorNotFound, err.Error())
 		cancel()
 	}
 
-	if err := auth.Enforce(
-		s.Senv,
-		atk,
-		r.ID,
-		rsc.Access,
-		rsc.AccessUser,
-	); err != nil {
-		e.Append(cons.ErrorAuth, err.Error())
+	// Enforce access requirements
+	if acc.Type == rsc.AccessService.String() {
+		// service is not allowed to update access
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s is not authorized to update access", acc.Type))
+		cancel()
+	} else if acc.Type == rsc.AccessUser.String() && acc.ID != r.ID {
+		// user is only allowed to update self
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s is only authorized to update self", acc.Type))
+		cancel()
+	} else if acc.Type == rsc.AccessAdmin.String() && r.Scope == rsc.AccessScopeWorkspace.String() && acc.WorkspaceKey != r.WorkspaceKey {
+		// admin scoped to workspace is only allowed update scoped workspace access
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s scoped to workspace %s is only authorized to update access scoped in the same workspace", acc.Type, acc.WorkspaceKey))
+		cancel()
+	} else if acc.Type == rsc.AccessAdmin.String() && r.Scope == rsc.AccessScopeProject.String() && (acc.WorkspaceKey != r.WorkspaceKey || acc.ProjectKey != r.ProjectKey) {
+		// admin scoped to workspace is only allowed update scoped workspace access
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s scoped to workspace %s and project %s is only authorized to update access scoped in the same workspace and project", acc.Type, acc.WorkspaceKey, acc.ProjectKey))
 		cancel()
 	}
+	// otherwise
+	// * update if root with instance scope
 
 	if err := patch.Transform(r, patchDoc, &o); err != nil {
 		e.Append(cons.ErrorInternal, err.Error())
@@ -270,22 +324,39 @@ func (s *Service) Delete(
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// Verify access is authorized
+	acc, err := authv2.Authorize(s.Senv, atk)
+	if err != nil {
+		e.Append(cons.ErrorAuth, err.Error())
+		return &e
+	}
+
 	r, err := s.AccessRepo.Get(ctx, accessmodel.KeySecretPair{Key: a.AccessKey.String()})
 	if err != nil {
 		e.Append(cons.ErrorNotFound, err.Error())
 		cancel()
 	}
 
-	if err := auth.Enforce(
-		s.Senv,
-		atk,
-		r.ID,
-		rsc.Workspace,
-		rsc.AccessAdmin,
-	); err != nil {
-		e.Append(cons.ErrorAuth, err.Error())
+	// Enforce access requirements
+	if acc.Type == rsc.AccessService.String() {
+		// service is not allowed to delete access
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s is not authorized to delete access", acc.Type))
+		cancel()
+	} else if acc.Type == rsc.AccessUser.String() && acc.ID != r.ID {
+		// user is only allowed to delete self
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s is only authorized to delete self", acc.Type))
+		cancel()
+	} else if acc.Type == rsc.AccessAdmin.String() && r.Scope == rsc.AccessScopeWorkspace.String() && acc.WorkspaceKey != r.WorkspaceKey {
+		// admin scoped to workspace is only allowed delete scoped workspace access
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s scoped to workspace %s is only authorized to delete access scoped in the same workspace", acc.Type, acc.WorkspaceKey))
+		cancel()
+	} else if acc.Type == rsc.AccessAdmin.String() && r.Scope == rsc.AccessScopeProject.String() && (acc.WorkspaceKey != r.WorkspaceKey || acc.ProjectKey != r.ProjectKey) {
+		// admin scoped to workspace is only allowed delete scoped workspace access
+		e.Append(cons.ErrorAuth, fmt.Sprintf("Access type %s scoped to workspace %s and project %s is only authorized to delete access scoped in the same workspace and project", acc.Type, acc.WorkspaceKey, acc.ProjectKey))
 		cancel()
 	}
+	// otherwise
+	// * delete if root with instance scope
 
 	if err := s.AccessRepo.Delete(ctx, a); err != nil {
 		e.Append(cons.ErrorInternal, err.Error())
