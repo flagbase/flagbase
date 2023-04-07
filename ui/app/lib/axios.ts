f3b6eadd8847cce781253d667215fbef1afa9f68
ali <aligatosan@gmail.com>
2023-04-07 12:57:10 +1000

    eslint updates and refactors

--- ui/app/lib/axios.ts
+++ ui/app/lib/axios.ts
@@ -1,58 +1,63 @@
-import Axios from 'axios'
-import { Instance } from '../context/instance'
-import { fetchAccessToken } from '../pages/workspaces/api'
-import { getInstances } from '../router/loaders'
-import { queryClient } from '../router/router'
+import Axios from 'axios';
+import { Instance } from '../context/instance';
+import { fetchAccessToken } from '../pages/workspaces/api';
+import { getInstances } from '../router/loaders';
+import { queryClient } from '../router/router';
 
-export const axios = Axios.create()
+export const axios = Axios.create();
 
 axios.interceptors.response.use(
-    (response) => {
-        return response.data
-    },
-    (error) => {
-        const message = error.response?.data?.message || error.message
-        return Promise.reject({
-            message,
-            status: error.response?.status || 404,
-        })
-    }
-)
-axios.defaults.baseURL = 'http://localhost:3000/'
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    const message = error.response?.data?.message || error.message;
+    return Promise.reject({
+      message,
+      status: error.response?.status || 404,
+    });
+  },
+);
+axios.defaults.baseURL = 'http://localhost:3000/';
 
-const getCachedAccessToken = async (connectionString: string, accessKey: string, accessSecret: string) => {
-    const hashKey = `${connectionString}-${accessKey}-${accessSecret}`
-    const cachedAccessToken = sessionStorage.getItem(hashKey)
-    if (cachedAccessToken) {
-        return {
-            accessToken: cachedAccessToken,
-        }
-    } else {
-        const { accessToken } = await fetchAccessToken(accessKey, accessSecret)
-        sessionStorage.setItem(hashKey, accessToken)
-        return { accessToken }
-    }
-}
+const getCachedAccessToken = async (
+  connectionString: string,
+  accessKey: string,
+  accessSecret: string,
+) => {
+  const hashKey = `${connectionString}-${accessKey}-${accessSecret}`;
+  const cachedAccessToken = sessionStorage.getItem(hashKey);
+  if (cachedAccessToken) {
+    return {
+      accessToken: cachedAccessToken,
+    };
+  } else {
+    const { accessToken } = await fetchAccessToken(accessKey, accessSecret);
+    sessionStorage.setItem(hashKey, accessToken);
+    return { accessToken };
+  }
+};
 
 export const configureAxios = async (instanceKey: string) => {
-    const instances = await queryClient.fetchQuery({
-        queryKey: ['instances'],
-        queryFn: () => getInstances(),
-    })
-    const instance = instances?.find(
-        (instance: Instance) => instance.key.toLocaleLowerCase() === instanceKey.toLocaleLowerCase()
-    )
-    if (!instance) {
-        // TODO: handle instance not found issue better
-        window.location.replace('/instances')
-        throw new Error('Instance not found')
-    }
+  const instances = await queryClient.fetchQuery({
+    queryKey: ['instances'],
+    queryFn: () => getInstances(),
+  });
+  const instance = instances?.find(
+    (instance: Instance) =>
+      instance.key.toLocaleLowerCase() === instanceKey.toLocaleLowerCase(),
+  );
+  if (!instance) {
+    // TODO: handle instance not found issue better
+    window.location.replace('/instances');
+    throw new Error('Instance not found');
+  }
 
-    axios.defaults.baseURL = instance?.connectionString
-    const { accessToken } = await getCachedAccessToken(
-        instance.connectionString,
-        instance.accessKey,
-        instance.accessSecret
-    )
-    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
-}
+  axios.defaults.baseURL = instance?.connectionString;
+  const { accessToken } = await getCachedAccessToken(
+    instance.connectionString,
+    instance.accessKey,
+    instance.accessSecret,
+  );
+  axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+};
--- ui/app/pages/instances/instances.test.tsx
+++ ui/app/pages/instances/instances.test.tsx
@@ -1,58 +1,63 @@
-import { expect, test } from '@playwright/experimental-ct-react'
-import { strToImagePath } from '../../../helpers'
+import { expect, test } from '@playwright/experimental-ct-react';
+import { strToImagePath } from '../../../helpers';
 
 test.afterEach(async ({ page }, testInfo) => {
-    await testInfo.attach(strToImagePath(testInfo.title), {
-        body: await page.screenshot(),
-        contentType: 'image/png',
-    })
-})
+  await testInfo.attach(strToImagePath(testInfo.title), {
+    body: await page.screenshot(),
+    contentType: 'image/png',
+  });
+});
 
 test('has title', async ({ page }) => {
-    await page.goto('http://localhost:3000')
+  await page.goto('http://localhost:3000');
 
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Instances | Flagbase/)
-})
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Instances | Flagbase/);
+});
 
 test('new user sees onboarding modal', async ({ page }) => {
-    await page.goto('http://localhost:3000/instances')
-    expect(page.getByText('Flagbase Client')).toBeTruthy()
-})
+  await page.goto('http://localhost:3000/instances');
+  expect(page.getByText('Flagbase Client')).toBeTruthy();
+});
 
 test('existing user does not see onboarding modal', async ({ page }) => {
-    await page.goto('http://localhost:3000/instances')
-    await expect(page.getByRole('heading', { name: 'Flagbase Client' }), 'modal text should be visible').toBeVisible()
+  await page.goto('http://localhost:3000/instances');
+  await expect(
+    page.getByRole('heading', { name: 'Flagbase Client' }),
+    'modal text should be visible',
+  ).toBeVisible();
 
-    await page.locator('#remember-me').check()
-    await page.getByRole('button', { name: 'Close' }).click()
-    await page.reload({ waitUntil: 'domcontentloaded' })
-    await expect(
-        page.getByRole('heading', { name: 'Flagbase Client' }),
-        'modal text should be visible'
-    ).not.toBeVisible()
-})
+  await page.locator('#remember-me').check();
+  await page.getByRole('button', { name: 'Close' }).click();
+  await page.reload({ waitUntil: 'domcontentloaded' });
+  await expect(
+    page.getByRole('heading', { name: 'Flagbase Client' }),
+    'modal text should be visible',
+  ).not.toBeVisible();
+});
 
 test('user can connect to an instance', async ({ page }) => {
-    await page.goto('http://localhost:3000/instances')
-    await page.locator('#remember-me').check()
-    await page.getByRole('button', { name: 'Close' }).click()
-    await page.getByRole('button', { name: 'Join instance', exact: true }).click()
-    await page.getByPlaceholder('Flagbase Instance').fill('Flagbase Local')
-    await page.getByPlaceholder('Flagbase Instance').press('Tab')
-    await page.getByPlaceholder('flagbase-instance').press('Tab')
-    await page.getByPlaceholder('URL').fill('http')
-    await page.getByPlaceholder('URL').press('Meta+a')
-    await page.getByPlaceholder('URL').click()
-    await page.getByPlaceholder('URL').press('Meta+a')
-    await page.getByPlaceholder('URL').fill('https://api.core.flagbase.xyz')
-    await page.getByPlaceholder('URL').press('Tab')
-    await page.getByPlaceholder('Key').fill('flagbase-root-access-343fdf')
-    await page.getByPlaceholder('Key').press('Tab')
-    await page.getByPlaceholder('Secret').fill('G0L4bNFfadfsdf')
-    await page.getByRole('button', { name: 'Add Instance' }).click()
-    await page.getByPlaceholder('flagbase-instance').click()
-    await page.getByPlaceholder('flagbase-instance').fill('flagbase')
-    await page.getByRole('button', { name: 'Add Instance' }).click()
-    await page.waitForTimeout(5000)
-})
+  await page.goto('http://localhost:3000/instances');
+  await page.locator('#remember-me').check();
+  await page.getByRole('button', { name: 'Close' }).click();
+  await page
+    .getByRole('button', { name: 'Join instance', exact: true })
+    .click();
+  await page.getByPlaceholder('Flagbase Instance').fill('Flagbase Local');
+  await page.getByPlaceholder('Flagbase Instance').press('Tab');
+  await page.getByPlaceholder('flagbase-instance').press('Tab');
+  await page.getByPlaceholder('URL').fill('http');
+  await page.getByPlaceholder('URL').press('Meta+a');
+  await page.getByPlaceholder('URL').click();
+  await page.getByPlaceholder('URL').press('Meta+a');
+  await page.getByPlaceholder('URL').fill('https://api.core.flagbase.xyz');
+  await page.getByPlaceholder('URL').press('Tab');
+  await page.getByPlaceholder('Key').fill('flagbase-root-access-343fdf');
+  await page.getByPlaceholder('Key').press('Tab');
+  await page.getByPlaceholder('Secret').fill('G0L4bNFfadfsdf');
+  await page.getByRole('button', { name: 'Add Instance' }).click();
+  await page.getByPlaceholder('flagbase-instance').click();
+  await page.getByPlaceholder('flagbase-instance').fill('flagbase');
+  await page.getByRole('button', { name: 'Add Instance' }).click();
+  await page.waitForTimeout(5000);
+});
