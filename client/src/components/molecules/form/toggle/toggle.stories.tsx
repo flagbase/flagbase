99cac76de8ff6228c7e80f83acfcdfbb3851fc25
ali <ali@infinibit.com.au>
2023-09-20 01:10:17 +1000

    use scenarios

--- client/src/components/molecules/form/toggle/toggle.stories.tsx
+++ client/src/components/molecules/form/toggle/toggle.stories.tsx
@@ -1,90 +1,18 @@
-// Toggle.stories.ts|tsx
-
 import React from 'react';
 
 import type { Meta, StoryObj } from '@storybook/react';
-import { Form, Formik } from 'formik';
 
 import Toggle from './toggle';
+import { scenarios } from './toggle.scenarios';
 
 const meta: Meta<typeof Toggle> = {
   component: Toggle,
 };
 
 export default meta;
-type Story = StoryObj<typeof Toggle>;
 
-export const Primary: Story = {
-  render: () => (
-    <Formik
-      initialValues={{
-        toggle: false,
-      }}
-      onSubmit={() => {}}
-    >
-      <Form>
-        <Toggle name="toggle" />
-      </Form>
-    </Formik>
-  ),
-};
-
-export const WithLabel: Story = {
-  render: () => (
-    <Formik
-      initialValues={{
-        toggle: false,
-      }}
-      onSubmit={() => {}}
-    >
-      <Form>
-        <Toggle name="toggle" label="Toggle" />
-      </Form>
-    </Formik>
-  ),
-};
-
-export const Checked: Story = {
-  render: () => (
-    <Formik
-      initialValues={{
-        toggle: true,
-      }}
-      onSubmit={() => {}}
-    >
-      <Form>
-        <Toggle name="toggle" label="Toggle" />
-      </Form>
-    </Formik>
-  ),
-};
-
-export const Unchecked: Story = {
-  render: () => (
-    <Formik
-      initialValues={{
-        toggle: false,
-      }}
-      onSubmit={() => {}}
-    >
-      <Form>
-        <Toggle name="toggle" label="Toggle" />
-      </Form>
-    </Formik>
-  ),
-};
-
-export const Disabled: Story = {
-  render: () => (
-    <Formik
-      initialValues={{
-        toggle: true,
-      }}
-      onSubmit={() => {}}
-    >
-      <Form>
-        <Toggle name="toggle" label="Toggle" disabled />
-      </Form>
-    </Formik>
-  ),
-};
+export const Primary = scenarios.Primary;
+export const WithLabel = scenarios.WithLabel;
+export const Checked = scenarios.Checked;
+export const Unchecked = scenarios.Unchecked;
+export const Disabled = scenarios.Disabled;
